9cfc0f73e0e829edd80ffd731d7c4941e575a6e3
L2Gmz <88688850+L2Gmz@users.noreply.github.com>
2025-07-15 16:45:46 +0200

    Create kpi.tsx
    Add page kpi.tsx

---
+++ src/pages/kpi.tsx
@@ -0,0 +1,8 @@
+export default function KPI() {
+  return (
+    <section class="bg-red-100 text-slate-700 p-8 rounded-md">
+      <h2 class="text-2xl">Key Performance Indicators</h2>
+      <p class="mt-4">How are we doing? 📶 ⬆️ ⬇️</p>
+    </section>
+  );
+}
